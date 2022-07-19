8ca08e5c27471fc1d598ab29c2f5c7545ee94325
The Grox Empire <47816228+TheGroxEmpire@users.noreply.github.com>
2022-07-19 19:55:23 +0700

    Another reward tweak

--- game.py
+++ game.py
@@ -667,7 +667,7 @@ class Game():
                 if team == 'attacker':
                     # --- Rewards for city status
                         if obj[1].status == 'dead':
-                            reward += 50
+                            reward += 10
                             obj[1].status = None
                         elif obj[1].status == 'took damage':
                             reward += 0.5
