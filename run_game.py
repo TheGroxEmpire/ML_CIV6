1d952279e47f43523f398c5cdc5fc4f8161b8dc3
The Grox Empire <47816228+TheGroxEmpire@users.noreply.github.com>
2022-08-03 21:13:33 +0700

    Add back defender agent

--- run_game.py
+++ run_game.py
@@ -14,7 +14,6 @@ import dueling_ddqn
 
 if __name__ == '__main__':
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
     # --- Load / save setting
     enable_load = False
     enable_save = True
@@ -29,7 +28,7 @@ if __name__ == '__main__':
     '''
     algorithm_version = 'dueling_ddqn'
 
-    comment_suffix = "a(3w2s)-d(0)_default"
+    comment_suffix = "a(3w2s)-d(2w1s)_default"
 
     env = gym_env.GymEnv("hide")
     env.reset()
@@ -50,9 +49,9 @@ if __name__ == '__main__':
         'ppo': ppo.Agent,
     }
     # Attacker action space
-    attacker_agent = algorithm_dict[algorithm_version](state, 7, lr=0.005, batch_size=256, replace=1000)
+    attacker_agent = algorithm_dict[algorithm_version](state, 7)
     # Defender action space
-    #defender_agent  = algorithm_dict[algorithm_version](state, 7, lr=0.005, batch_size=256, replace=1000)
+    defender_agent  = algorithm_dict[algorithm_version](state, 7)
 
     if enable_save:
         if not os.path.exists(f"./logs/{algorithm_version}_{comment_suffix}"):
@@ -68,7 +67,7 @@ if __name__ == '__main__':
         # Cumulative episodes is the number of episode from previous load. If there's nothing to load, start at 0
         cumulative_episodes = len(attacker_r)
         attacker_agent.load_model(f'attacker_{algorithm_version}_{comment_suffix}')
-        #defender_agent.load_model(f'defender_{algorithm_version}_{comment_suffix}')
+        defender_agent.load_model(f'defender_{algorithm_version}_{comment_suffix}')
         print("Continuing from last save data")
        
     else:
@@ -88,9 +87,9 @@ if __name__ == '__main__':
         for step in range(N_TURNS):
             done = False
             attacker_end_turn = False
-            #defender_end_turn = False
+            defender_end_turn = False
             attacker_reward_episode = 0
-            #defender_reward_episode = 0
+            defender_reward_episode = 0
             # print(f"Attacker turn")
             while True:
                 if done or attacker_end_turn:
@@ -107,30 +106,30 @@ if __name__ == '__main__':
                 # --- Store reward for plots
                 attacker_reward_episode += attacker_reward
                 
-            # # print(f"Defender turn")
-            # while True:
-            #     if done or defender_end_turn:
-            #         break
-            #      # --- Determine what action to take. 
-            #     defender_action = defender_agent.act(state)
-            #     # --- Perform that action in the environment
-            #     # print(f"Defender action: {defender_action}, turn: {step}")
-            #     next_state, defender_reward, defender_end_turn, done = env.step('defender', defender_action)
-            #     # --- Store state and action into memory
-            #     defender_agent.remember(state, next_state, defender_action, defender_reward, done)
-            #     # --- Update the current state of the game
-            #     state = np.array(next_state)
-            #     defender_reward_episode += defender_reward
+            # print(f"Defender turn")
+            while True:
+                if done or defender_end_turn:
+                    break
+                 # --- Determine what action to take. 
+                defender_action = defender_agent.act(state)
+                # --- Perform that action in the environment
+                # print(f"Defender action: {defender_action}, turn: {step}")
+                next_state, defender_reward, defender_end_turn, done = env.step('defender', defender_action)
+                # --- Store state and action into memory
+                defender_agent.remember(state, next_state, defender_action, defender_reward, done)
+                # --- Update the current state of the game
+                state = np.array(next_state)
+                defender_reward_episode += defender_reward
 
             if done:
                 break
         
         # --- Replay the agent past experience
         attacker_agent.replay()
-        #defender_agent.replay()
+        defender_agent.replay()
         # --- Store latest reward
         attacker_r = np.append(attacker_r, attacker_reward_episode)
-        #defender_r = np.append(defender_r, defender_reward_episode) 
+        defender_r = np.append(defender_r, defender_reward_episode) 
         # --- Get end time
         e = time.time()
         print(f"Episode: {epoch}, time spent: {round(e-s, 2)}s")
@@ -139,16 +138,16 @@ if __name__ == '__main__':
         # --- Save model and data value every 100 episodes or at the last episode
         if enable_save and (epoch % 1000 == 0 or epoch == episode_end-1):
             attacker_agent.save_model(f'attacker_{algorithm_version}_{comment_suffix}')
-            #defender_agent.save_model(f'defender_{algorithm_version}_{comment_suffix}')
+            defender_agent.save_model(f'defender_{algorithm_version}_{comment_suffix}')
 
             with open(f"./plots/{algorithm_version}_{comment_suffix}.csv", 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(attacker_r)
-                #writer.writerow(defender_r)
+                writer.writerow(defender_r)
 
             with tensorboard_writer.as_default():
                 tf.summary.scalar(name=f"attacker_reward", data=np.mean(attacker_r[-1000:]), step=epoch)
-                #tf.summary.scalar(name=f"defender_reward", data=np.mean(defender_r[-1000:]), step=epoch)
+                tf.summary.scalar(name=f"defender_reward", data=np.mean(defender_r[-1000:]), step=epoch)
                 tensorboard_writer.flush()
 
     env.close()    
@@ -161,7 +160,7 @@ if __name__ == '__main__':
         return avg
 
     plt.plot(moving_average(attacker_r), label='Attacker rewards')
-    #plt.plot(moving_average(defender_r), label='Defender rewards')
+    plt.plot(moving_average(defender_r), label='Defender rewards')
     plt.title(f"{algorithm_version}_{comment_suffix}")
     plt.legend() 
     if enable_save:
